1ac9e7f9b8ebc1574bda81fcd6723b80495f3b22
MUKILTEO\1713915 <1713915@mukilteo.wednet.edu>
2023-02-17 10:14:23 -0800

    Nothing

--- CRPGVanOrnam/Item.cs
+++ CRPGVanOrnam/Item.cs
@@ -6,5 +6,15 @@ namespace CRPGVanOrnam
 {
     class Item
     {
+        public int ID;
+        public string Name;
+        public string NamePlural;
+
+        public Item(int iD, string name, string namePlural)
+        {
+            ID = iD;
+            Name = name;
+            NamePlural = namePlural;
+        }
     }
 }
